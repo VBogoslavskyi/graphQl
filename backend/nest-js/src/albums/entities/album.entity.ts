cc5a193ae94cbb7726a68290125b55664519570d
Valentyn Bohoslavskyi <vbohoslavskyi@jappware.com>
2023-11-26 20:24:33 +0200

    Update table name for DB

--- backend/nest-js/src/albums/entities/album.entity.ts
+++ backend/nest-js/src/albums/entities/album.entity.ts
@@ -2,7 +2,7 @@ import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
-@Entity('Albums')
+@Entity('albums')
 export class AlbumEntity {
   @Field(() => ID)
   @PrimaryGeneratedColumn()
